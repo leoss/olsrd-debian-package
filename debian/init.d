a9c5b82a1bb1575f141626f905e834acdb903b45
Holger Levsen <debian@layer-acht.org>
2006-12-20 22:57:21 +0000

    Import Debian changes 0.4.10-3
    olsrd (0.4.10-3) unstable; urgency=low
    
      * improved Makefile clean target to allow rebuilds
      * fixed and improved broken /etc/default/olsrd and /etc/init.d/olsrd
      * improved README.Debian and olsrd.conf

--- debian/init.d
+++ debian/init.d
@@ -14,7 +14,7 @@
 #		Modified for olsrd
 #		by Holger Levsen <debian@layer-acht.org>
 # 
-# Version:	09-Dec-2006  
+# Version:	21-Dec-2006  
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 DAEMON=/usr/sbin/olsrd
@@ -28,48 +28,34 @@ if [ -f /etc/default/olsrd ] ; then
 	. /etc/default/olsrd
 fi
 
-if [ "$START_OLSRD" != "YES" ] ; then exit 0 ; fi
-
 set -e
 
 case "$1" in
-  start)
+  debug)
 	echo -n "Starting $DESC: "
-	start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON -- $DAEMON_OPTS &
+	start-stop-daemon --start --quiet --exec $DAEMON -- $DAEMON_OPTS &
+	echo "$NAME."
+	;;
+  start)
+	if [ "$START_OLSRD" != "YES" ] ; then exit 0 ; fi
+	echo -n "Starting $DESC: "
+	start-stop-daemon --start --quiet --exec $DAEMON -- $DAEMON_OPTS &
 	echo "$NAME."
 	;;
   stop)
 	echo -n "Stopping $DESC: "
-	start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON
+	killall $DAEMON
 	echo "$NAME."
 	;;
-  reload|force-reload)
-	#
-	#	If the "reload" option is implemented, move the "force-reload"
-	#	option to the "reload" entry above. If not, "force-reload" is
-	#	just the same as "restart" except that it does nothing if the
-	#   daemon isn't already running.
-	# check wether $DAEMON is running. If so, restart
-	start-stop-daemon --stop --test --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON \
-	&& $0 restart \
-	|| exit 0
-	;;
-  restart)
-    echo -n "Restarting $DESC: "
-	start-stop-daemon --stop --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON
-	sleep 1
-	start-stop-daemon --start --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS
+  restart|force-reload)
+	echo -n "Restarting $DESC: "
+	killall $DAEMON & sleep 1
+	start-stop-daemon --start --quiet --exec $DAEMON -- $DAEMON_OPTS
 	echo "$NAME."
 	;;
   *)
 	N=/etc/init.d/$NAME
-	# echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $N {start|stop|restart|force-reload}" >&2
+	echo "Usage: $N {start|stop|restart}" >&2
 	exit 1
 	;;
 esac
